a518d37732386f28e3abd36c307a4f6c2b0dc31e
rakib hussain <rakibhussain8812@gmail.com>
2026-01-17 16:52:01 +0530

    learning stdout,stderr and  close function

--- index.js
+++ index.js
@@ -2,4 +2,23 @@
 //here process.argvs parse the commands in 3 parts , node executable path, path of js file which runs and then at last it prints the 
 console.log(process.argv);
 
+//lets understand what is spawn and how it works
+
+const {spawn}=require('child_process');
+
+
+const start=Date.now();
+
+const child=spawn('node',['-v'],{shell:true}); 
+
+child.stdout.on('data',d=>process.stdout.write(d));
+child.stderr.on('data',d=>process.stderr.write(d));
+
+child.on('close',(code)=>{
+    const end=Date.now();
+    console.log('---');
+    console.log('Exit code:',code);
+    console.log('Time:',start-end,'ms')
+});
+
 
